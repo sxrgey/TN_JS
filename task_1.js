f7b274686bc1591c1297f91e48fe5d6aa17e2cd6
sxrgey <serezha_vin_19@mail.ru>
2023-06-20 17:58:45 +0300

    feat: task 1 is done

--- task_1.js
+++ task_1.js
@@ -1,1 +1,14 @@
-console.log("test");
+let arr = [1, 2, 3, 4, 2, 1, 5, 6, 4, 5];
+
+const uniqueValues = (arr) => {
+  let new_arr = [];
+  arr.forEach(num => {
+    if (!(num in new_arr)) {
+      new_arr.push(num);
+    }
+  });
+  return new_arr;
+}
+
+console.log(arr);
+console.log(uniqueValues(arr));
